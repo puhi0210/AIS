fe40e4067cef3d0ce7d94971033fc728bc5b71dc
puhi0210 <mihael0210@gmail.com>
2024-10-22 22:32:46 +0200

    vaja1

--- labVaje/vaja1/AIS_assignment1.py
+++ labVaje/vaja1/AIS_assignment1.py
@@ -1,4 +1,10 @@
 import cv2
+from mtcnn import MTCNN
+import copy
+
+
+# Initialize the MTCNN detector
+detector = MTCNN()
 
 # Open the camera at the ID 0 
 capture = cv2.VideoCapture(0)
@@ -21,7 +27,7 @@ while(True):
     cv2.imshow("Camera is live :)",frame)
 
     # NALOGA 2
-
+    
     ## Convert the frame to GRAYSCALE
     # Grayscale images are easier to process and are often used in image analysis.
     grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -73,6 +79,49 @@ while(True):
     # Pixels above the threshold (120) are set to white (255), and others are set to black (0).
     ret, thresholdFrame = cv2.threshold(HCGrayFrame, 120, 255, cv2.THRESH_BINARY)
     cv2.imshow("Thresholding",thresholdFrame)
+
+
+    ## NALOGA 3
+
+    # FACE DETECTION 
+
+    frame_faces = copy.deepcopy(frame)
+    #frame_faces = cv2.imread("/home/miha/Projects/AIS/labVaje/vaja1/data/oscar.jpg")
+    grayFrame_faces = cv2.cvtColor(frame_faces,cv2.COLOR_BGR2GRAY)
+    # Nalaganje prednatreniranega modela
+    face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
+    # haarcascade_frontalface_alt
+    #face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_profileface.xml")
+    # Detekcija obraza
+    faces = face_cascade.detectMultiScale(
+        grayFrame_faces,               # Input image (grayscale)
+        scaleFactor=1.1,    # Scale factor for image reduction
+        minNeighbors=5,     # Number of neighbors to retain a detectionhaarcascade_frontalface_alt
+        minSize=(30, 30),   # Minimum size of face to detect (optional)
+    )
+    # Draw rectangles around detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame_faces, (x, y), (x+w, y+h), (255, 0, 255), 2)
+    # Show the image with the detected faces
+    cv2.imshow('Face Detection', frame_faces)
+
+
+    ## NALOGA 4
+
+    # FACE DETECTION with MTCNN
+
+    #frame_faces_mtcnn = copy.deepcopy(frame)
+    frame_faces_mtcnn = cv2.imread("/home/miha/Projects/AIS/labVaje/vaja1/data/oscar.jpg")
+    # Detect faces in the frame
+    faces_mtcnn = detector.detect_faces(frame_faces_mtcnn)
+
+    # Draw bounding boxes around detected faces
+    for face in faces_mtcnn:
+        x, y, width, height = face['box']
+        cv2.rectangle(frame_faces_mtcnn, (x, y), (x + width, y + height), (255, 0, 0), 2)
+
+    # Display the frame with detected faces
+    cv2.imshow('Real-Time Face Detection (MTCNN)', frame_faces_mtcnn)
 
 
 
